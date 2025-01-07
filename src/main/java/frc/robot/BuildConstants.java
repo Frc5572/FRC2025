c961fd95e7c52e277da1e5e8a9b9274132b115cb
Gael Valdez <gvaldez7206@stu.neisd.net>
2025-01-07 16:13:52 -0600

    fixed linting errors

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -4,16 +4,16 @@ package frc.robot;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-	public static final String MAVEN_GROUP = "";
-	public static final String MAVEN_NAME = "FRC2025";
-	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 4;
-	public static final String GIT_SHA = "9953db9ad492ff47c6df1fc74ebf8d5d9324e093";
-	public static final String GIT_DATE = "2025-01-06 17:06:34 EST";
-	public static final String GIT_BRANCH = "updateTo2025";
-	public static final String BUILD_DATE = "2025-01-07 16:32:58 EST";
-	public static final long BUILD_UNIX_TIME = 1736285578652L;
-	public static final int DIRTY = 1;
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "FRC2025";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 4;
+    public static final String GIT_SHA = "9953db9ad492ff47c6df1fc74ebf8d5d9324e093";
+    public static final String GIT_DATE = "2025-01-06 17:06:34 EST";
+    public static final String GIT_BRANCH = "updateTo2025";
+    public static final String BUILD_DATE = "2025-01-07 16:32:58 EST";
+    public static final long BUILD_UNIX_TIME = 1736285578652L;
+    public static final int DIRTY = 1;
 
-	private BuildConstants() {}
+    private BuildConstants() {}
 }
