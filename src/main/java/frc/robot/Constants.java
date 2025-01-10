838f37103e1c991b7e4e233f724cebe05ffd3bdb
Gael Valdez <gvaldez7206@stu.neisd.net>
2025-01-10 09:48:04 -0600

    just setting up the swerve values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+
 /**
  * Constants file.
  */
@@ -23,6 +25,56 @@ public final class Constants {
     public static final class Motors {
     }
 
+    /**
+     * Swerve Constants
+     */
+    public static final class Swerve {
+
+        /* Module Specific Constants */
+
+        /**
+         * Front Left Module - Module 0
+         */
+        public static final class Mod0 {
+            public static final int driveMotorID = 1;
+            public static final int angleMotorID = 5;
+            public static final int canCoderID = 1;
+            public static final Rotation2d angleOffset = Rotation2d.fromRotations(0.421631);
+
+        }
+
+        /**
+         * Front Right Module - Module 1
+         */
+        public static final class Mod1 {
+            public static final int driveMotorID = 2;
+            public static final int angleMotorID = 6;
+            public static final int canCoderID = 2;
+            public static final Rotation2d angleOffset = Rotation2d.fromRotations(-0.083984);
+
+        }
+
+        /**
+         * Back Left Module - Module 2
+         */
+        public static final class Mod2 {
+            public static final int driveMotorID = 4;
+            public static final int angleMotorID = 8;
+            public static final int canCoderID = 4;
+            public static final Rotation2d angleOffset = Rotation2d.fromRotations(0.172119);
+
+        }
+
+        /**
+         * Back Right Module - Module 3
+         */
+        public static final class Mod3 {
+            public static final int driveMotorID = 3;
+            public static final int angleMotorID = 7;
+            public static final int canCoderID = 3;
+            public static final Rotation2d angleOffset = Rotation2d.fromRotations(0.177734);
+        }
+    }
     /**
      * Pneumatics CAN id constants.
      */
