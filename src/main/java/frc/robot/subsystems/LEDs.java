c73a588feee54729c622922bd7a3e00245711ed7
Merridew1 <jrmullettii@outlook.com>
2025-01-23 21:29:43 -0600

    made leds extend subsystembase

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -6,9 +6,10 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class LEDs {
+public class LEDs extends SubsystemBase {
     private final AddressableLED leds;
     private final AddressableLEDBuffer buffer;
 
