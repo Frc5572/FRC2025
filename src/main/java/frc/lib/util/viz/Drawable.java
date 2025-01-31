d782987a9721b902e9af24de24258c06e88a9047
wilsonwatson <wilsonwatson@utexas.edu>
2025-01-30 21:36:37 -0600

    linting

--- src/main/java/frc/lib/util/viz/Drawable.java
+++ src/main/java/frc/lib/util/viz/Drawable.java
@@ -5,9 +5,10 @@ import frc.robot.Constants;
 /** Something that can be drawn to AdvantageScope. */
 public interface Drawable {
 
-    /** Draw to AdvantageScope */
+    /** Draw to AdvantageScope unconditionally. */
     public void drawImpl();
 
+    /** Draw to AdvantageScope if drawing is enabled. */
     public default void draw() {
         if (Constants.shouldDrawStuff) {
             drawImpl();
