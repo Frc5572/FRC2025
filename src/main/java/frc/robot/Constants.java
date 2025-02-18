844cb89e04c4902bf63b8d02b38fd41a0c2db8ed
Reed Mullett <jrmullettii@outlook.com>
2025-02-18 17:58:42 -0600

    LEDs count changed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -106,7 +106,7 @@ public final class Constants {
      */
     public static final class LEDs {
         public static final int LED_PORT = 0;
-        public static final int LED_LENGTH = 123;
+        public static final int LED_LENGTH = 60;
     }
 
 
