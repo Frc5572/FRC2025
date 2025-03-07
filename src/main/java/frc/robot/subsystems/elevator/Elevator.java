ad5d8b4e1d25052ee1d72b3fa42b2d31b498ee3d
Gael <gvaldez7206@stu.neisd.net>
2025-03-07 08:14:32 -0600

    Update at event (Fri Mar 07 08:14:32 CST 2025)

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -60,7 +60,8 @@ public class Elevator extends SubsystemBase {
     public Command home() {
         Command slowLower = this.runEnd(() -> io.setVoltage(-1.4), () -> io.setVoltage(0.0));
         return moveTo(() -> Constants.Elevator.HOME).until(() -> inputs.position.in(Inches) < 5.0)
-            .andThen(slowLower.until(limitSwitchTouched)).andThen(this.runOnce(() -> {
+            .andThen(slowLower.until(limitSwitchTouched).withTimeout(2))
+            .andThen(this.runOnce(() -> {
                 io.resetHome();
                 Logger.recordOutput(Constants.Elevator.heightName, "home");
             }));
