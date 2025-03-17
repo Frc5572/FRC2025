27f4973eddc5c45b66f3ff7720ec74655a87b62a
Alex Resnick <adr8292@gmail.com>
2025-03-17 07:24:58 -0500

    Prevent trying to upload settings to simulated PV

--- src/main/java/frc/robot/subsystems/vision/VisionReal.java
+++ src/main/java/frc/robot/subsystems/vision/VisionReal.java
@@ -30,32 +30,8 @@ public class VisionReal implements VisionIO {
     public VisionReal(Constants.Vision.CameraConstants[] constants) {
         cameras = Stream.of(constants).map((consts) -> new PhotonCamera(consts.name()))
             .toArray(PhotonCamera[]::new);
-        for (String copro : coprocessorNames) {
-            new Thread(() -> {
-                Timer timer = new Timer();
-                boolean run = true;
-                int counter = 0;
-                timer.start();
-                while (run) {
-                    if (counter >= 12) {
-                        run = false;
-                    }
-                    if (timer.advanceIfElapsed(5.0)) {
-                        try {
-                            if (!waitForPV(copro)) {
-                                continue;
-                            }
-                            if (uploadAprilTagMap(copro)) {
-                                run = false;
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        counter++;
-                    }
-                    Thread.yield();
-                }
-            }).start();
+        for (String hostname : coprocessorNames) {
+            createSettingsUploadThread(hostname);
         }
 
     }
@@ -126,5 +102,33 @@ public class VisionReal implements VisionIO {
             }
         }
         return false;
+    }
+
+    protected void createSettingsUploadThread(String hostname) {
+        new Thread(() -> {
+            Timer timer = new Timer();
+            boolean run = true;
+            int counter = 0;
+            timer.start();
+            while (run) {
+                if (counter >= 12) {
+                    run = false;
+                }
+                if (timer.advanceIfElapsed(5.0)) {
+                    try {
+                        if (!waitForPV(hostname)) {
+                            continue;
+                        }
+                        if (uploadAprilTagMap(hostname)) {
+                            run = false;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    counter++;
+                }
+                Thread.yield();
+            }
+        }).start();
     }
 }
