6ab3ce57134ba5859806f1b54770f776d3e0e9a1
Alex Resnick <adr8292@gmail.com>
2025-03-18 10:36:29 -0500

    fix linting

--- src/main/java/frc/robot/subsystems/swerve/SwerveSim.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSim.java
@@ -8,12 +8,17 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.lib.util.swerve.SwerveModule;
 import frc.lib.util.swerve.SwerveModuleSim;
 
-/** Simulated Swerve Drive */
+/**
+ * Simulated Swerve Drive
+ */
 public class SwerveSim implements SwerveIO {
 
     private final GyroSimulation gyroSim;
     private final SwerveDriveSimulation simulation;
 
+    /**
+     * Simulated Swerve Drive
+     */
     public SwerveSim(SwerveDriveSimulation simulation) {
         this.gyroSim = simulation.getGyroSimulation();
         this.gyroSim.setRotation(Rotation2d.fromRotations(Math.random() - 0.5));
