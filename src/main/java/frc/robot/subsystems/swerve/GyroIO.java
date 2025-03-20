27d16e223704a7c569276d249edec048a7cea4b2
jace rodgers <jaransas2008@gmail.com>
2025-03-20 17:36:51 -0500

    lint 2

--- src/main/java/frc/robot/subsystems/swerve/GyroIO.java
+++ src/main/java/frc/robot/subsystems/swerve/GyroIO.java
@@ -2,8 +2,9 @@ package frc.robot.subsystems.swerve;
 
 import org.littletonrobotics.junction.AutoLog;
 
+/** Gyro IO layer */
 public interface GyroIO {
-
+    /** Gyro input logger */
     @AutoLog
     public class GyroInputs {
         double yaw;
@@ -13,6 +14,7 @@ public interface GyroIO {
 
     public void updateInputs(GyroInputs inputs);
 
+    /** Empty Swerve implementation (for replay) */
     public static class Empty implements GyroIO {
         @Override
         public void updateInputs(GyroInputs inputs) {};
