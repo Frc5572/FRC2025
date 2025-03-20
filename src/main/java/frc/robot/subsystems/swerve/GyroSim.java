53cd51ee7f6a0ba2645c1d3b998d407ef30a247b
jace rodgers <jaransas2008@gmail.com>
2025-03-20 12:36:11 -0500

    probaly sim

---
+++ src/main/java/frc/robot/subsystems/swerve/GyroSim.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.swerve;
+
+import org.ironmaple.simulation.drivesims.GyroSimulation;
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.lib.util.swerve.SwerveModule;
+
+public class GyroSim implements SwerveIO {
+
+    private GyroSimulation navXSim;
+    private GyroSimulation canandGyroSim;
+
+    public GyroSim(GyroSimulation navXSim, GyroSimulation canandGyroSim) {
+        this.navXSim = navXSim;
+        this.canandGyroSim = canandGyroSim;
+    }
+
+    @Override
+    public void updateInputs(SwerveInputs inputs) {
+        inputs.yaw = navXSim.getGyroReading().getRotations();
+        inputs.yaw = canandGyroSim.getGyroReading().getRotations();
+    }
+
+    @Override
+    public SwerveModule[] createModules() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createModules'");
+    }
+
+    @Override
+    public void setPose(Pose2d pose) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPose'");
+    }
+
+}
--- src/main/java/frc/robot/subsystems/swerve/SwerveSim.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSim.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.swerve;
 
-import org.ironmaple.simulation.drivesims.GyroSimulation;
 import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
 import org.ironmaple.simulation.drivesims.SwerveModuleSimulation;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -11,18 +10,14 @@ import frc.lib.util.swerve.SwerveModuleSim;
 /** Simulated Swerve Drive */
 public class SwerveSim implements SwerveIO {
 
-    private final GyroSimulation gyroSim;
     private final SwerveDriveSimulation simulation;
 
     public SwerveSim(SwerveDriveSimulation simulation) {
-        this.gyroSim = simulation.getGyroSimulation();
         this.simulation = simulation;
     }
 
     @Override
-    public void updateInputs(SwerveInputs inputs) {
-        inputs.yaw = gyroSim.getGyroReading().getRotations();
-    }
+    public void updateInputs(SwerveInputs inputs) {}
 
     @Override
     public SwerveModule[] createModules() {
