38ccc915984cdafe8b615c4921ed505ecf42562d
Jace Rodgers <jaransas2008@gmail.com>
2025-10-07 20:03:48 -0500

    worlds okest rosbot

--- src/main/java/frc/robot/subsystems/quest/Quest.java
+++ src/main/java/frc/robot/subsystems/quest/Quest.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems.quest;
 
+import static edu.wpi.first.units.Units.Inches;
 import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,6 +15,8 @@ public class Quest extends SubsystemBase {
     private QuestIO io;
     private QuestInputsAutoLogged inputs = new QuestInputsAutoLogged();
     private Pose2d posInit;
+    Transform2d robotToQuest = new Transform2d(new Translation2d(Inches.of(-11.2), Inches.of(9.75)),
+        Rotation2d.fromDegrees(-90)); // TRANSFORM INCORRECT NEEDS FIXING
     private RobotState state;
 
     public Quest(QuestIO io, RobotState state) {
@@ -35,7 +41,7 @@ public class Quest extends SubsystemBase {
     }
 
     public Pose2d getPose() {
-        return inputs.questPose;
+        return inputs.questPose.transformBy(robotToQuest);
     }
 
     public Command setPos() {
--- src/main/java/frc/robot/subsystems/quest/QuestReal.java
+++ src/main/java/frc/robot/subsystems/quest/QuestReal.java
@@ -1,10 +1,6 @@
 package frc.robot.subsystems.quest;
 
-import static edu.wpi.first.units.Units.Inches;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import gg.questnav.questnav.PoseFrame;
 import gg.questnav.questnav.QuestNav;
 
@@ -13,8 +9,7 @@ public class QuestReal implements QuestIO {
     // Get the latest pose data frames from the Quest
     PoseFrame[] poseFrames = quest.getAllUnreadPoseFrames();
     Pose2d pose;
-    Transform2d robotToQuest = new Transform2d(new Translation2d(Inches.of(-11.2), Inches.of(9.75)),
-        Rotation2d.kCCW_90deg);
+
 
     @Override
     public void updateInputs(QuestInputs inputs) {
@@ -23,7 +18,7 @@ public class QuestReal implements QuestIO {
         if (poseFrames.length > 0) {
             pose = poseFrames[poseFrames.length - 1].questPose();
 
-            inputs.questPose = pose.transformBy(robotToQuest.inverse());
+            inputs.questPose = pose;
             inputs.questYaw = poseFrames[poseFrames.length - 1].questPose().getRotation();
         }
         inputs.tracking = quest.isTracking();
